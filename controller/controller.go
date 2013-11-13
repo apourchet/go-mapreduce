af676dc655199b7cefff84a43c1447b246b3d79e
apourchet <antoine.pourchet@gmail.com>
2013-11-13 11:56:44 -0500

    Added some controller stuff

--- controller/controller.go
+++ controller/controller.go
@@ -1,1 +1,22 @@
 package controller
+
+import (
+	. "../socketio"
+	"../worker_manager"
+)
+
+func MapReturnMessage(fromRemote string) Message {
+	return Message{}
+}
+
+func CombineReturnMessage(fromRemote string) Message {
+	return Message{}
+}
+
+func ReduceReturnMessage(fromRemote string) Message {
+	return Message{}
+}
+
+func HandleMessage(message Message) {
+	fmt.Println("Controller Handling message:\n" + message.ToString())
+}
--- worker_manager/worker_manager.go
+++ worker_manager/worker_manager.go
@@ -1,9 +1,11 @@
 package workermanager
 
 import (
+	"../controller"
 	. "../socketio"
 )
 
+// Messages that will be handled by the Worker Manager
 func RunMessage(fromRemote, fileName string) Message {
 	return Message{fromRemote, RunType, "", "fileName:" + fileName}
 }
@@ -20,9 +22,21 @@ func RmFileMessage(fromRemote, fileName string) Message {
 	return Message{fromRemote, IOType, "", "fileName:" + fileName}
 }
 
+func MapJobMessage(fromRemote string) Message {
+
+}
+
+func CombineJobMessage(fromRemote string) Message {
+
+}
+
+func ReduceJobMessage(fromRemote string) Message {
+
+}
+
 // Does everything the message wants the worker to do
 // and creates a response to the controller that gives 
 // the status of the job
 func HandleMessage(message Message) {
-
+	os.OpenFile("foo.txt", os.O_RDWR|os.O_APPEND, 0660)
 }
